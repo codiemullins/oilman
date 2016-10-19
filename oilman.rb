171c78e46482dbed80cf69e84f1d720c813d2eac
Codie Mullins <codie.mullins@waterfield.com>
2016-10-19 08:48:47 -0500

    Allow traversal of directories after search

--- oilman.rb
+++ oilman.rb
@@ -18,22 +18,30 @@ def mount
   status = MOUNTER.mount PATH
 end
 
-def file_list file
+def file_list file, expand_path = false
   name = file.split("/").last
   return if name == "." || name == ".."
   directory = File.directory? file
+  return unless name.include?(".bak") || directory
 
-  {
-    name: name,
-    directory: directory,
-    size: File.size(file)
-  }
+  if directory && expand_path
+    dir_files = []
+    Dir.foreach(file) { |filename| dir_files.push *file_list("#{file}/#{filename}") }
+    dir_files
+  else
+    [{
+      name: file.gsub("#{PATH}/", ""),
+      directory: directory,
+      size: File.size(file)
+    }]
+  end
 end
 
-def list path = PATH, filter = ""
+def list path = PATH, filter = "", expand_path = false
   Dir.entries(path)
     .select { |entry| filter == "" ? true : entry =~ /#{filter}/i }
-    .map { |entry| file_list "#{path}/#{entry}" }
+    .map { |entry| file_list "#{path}/#{entry}", expand_path }
+    .flatten
     .compact
 end
 
@@ -50,23 +58,28 @@ def reduce_list filter = ""
 end
 
 mount
+
+def select_backup list
+  CLI.choose do |menu|
+    menu.prompt = "Please choose your backup:  "
+    list.each do |option|
+      menu.choice(option[:name]) do
+        CLI.say "===="
+        if option[:directory]
+          select_backup list "#{PATH}/#{option[:name]}", "", true
+        else
+          CLI.say "You've chosen a file... I don't support that yet"
+          option[:name]
+        end
+      end
+    end
+  end
+end
+
 backup_options = reduce_list default_filter
 
 puts ""
 
-choice = CLI.choose do |menu|
-  menu.prompt = "Please choose your backup:  "
-  backup_options.each do |option|
-    menu.choice(option[:name]) do
-      CLI.say "===="
-      if option[:directory]
-        ap list "#{PATH}/#{option[:name]}"
-      else
-        CLI.say "You've chosen a file... I don't support that yet"
-      end
-      option[:name]
-    end
-  end
-end
+choice = select_backup backup_options
 
 ap choice
