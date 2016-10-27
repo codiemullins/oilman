f15fb327b911dd6fa5b31f923a74b80232a0879a
Nathan Matthews <lowentropy@gmail.com>
2016-10-27 12:11:47 -0500

    More environment variables

--- lib/oilman/file_info.rb
+++ lib/oilman/file_info.rb
@@ -1,7 +1,7 @@
 class FileInfo
   attr_reader :name, :directory, :size
   def initialize file
-    @name = file.gsub("#{Settings[:mount][:path]}/", "")
+    @name = file.gsub("#{Settings[:mount][:local_path]}/", "")
     @directory = File.directory? file
     @size = File.size file
   end
